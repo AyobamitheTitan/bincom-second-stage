0db3845aca6b603c05b82d2c327a094217791456
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-10-22 15:57:42 +0100

    Completed the second set of tasks

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const prisma = require('./prisma')
+const express = require("express");
+const hbs = require("express-handlebars");
+const prisma = require("./prisma");
 
-const app = express()
+const app = express();
 
-app.engine('handlebars',hbs.engine())
-app.set('view engine','handlebars')
-app.set('views','./views')
+app.engine("handlebars", hbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
+app.get("/", async (req, res) => {
+  return res.render("home", { title: "Bincom Assignment" });
+});
 
-app.get('/',async (req,res)=>{
-    // const query = await prisma.$queryRawUnsafe(`SELECT * FROM polling_unit`)
-    // console.log(query);
-    return res.render('home',{title:"Bincom Assignment"})
-})
+app.get("/polling", (req, res) => {
+  return res.render("polling");
+});
 
-app.get('/polling',(req,res)=>{
-    return res.render('polling')
-})
+app.get("/polling/:id", async (req, res) => {
+  const query = await prisma.$queryRawUnsafe(
+    `SELECT * FROM polling_unit WHERE uniqueid = ${req.params.id}`
+  );
+  return res.status(200).json(query);
+});
 
-app.get('/polling/:id',async(req,res)=>{
-    const query = await prisma.$queryRawUnsafe(`SELECT * FROM polling_unit WHERE uniqueid = ${req.params.id}`)
-    // console.log(query);
-    return res.status(200).json(query)
-})
+app.get("/lga", (req, res) => {
+  return res.render("lga");
+});
 
-app.listen(3000,()=>{
-    console.log("Listening on port 3000");
-})
+app.get("/lga/:id", async (req, res) => {
+  const query = await prisma.$queryRawUnsafe(
+    `SELECT * FROM polling_unit WHERE lga_id = ${req.params.id}`
+  );
+  return res.status(200).json(query);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
